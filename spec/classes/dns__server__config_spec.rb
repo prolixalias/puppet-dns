815a98a513174070cba09310fec344001adb724a
Paul Talbot <prolixalias@gmail.com>
2020-03-17 08:50:01 -0400

    additional cleanup

--- spec/classes/dns__server__config_spec.rb
+++ spec/classes/dns__server__config_spec.rb
@@ -5,7 +5,7 @@ describe 'dns::server::config', type: :class do
     let :facts do
       {
         osfamily: 'Solaris',
-        concat_basedir: '/dne'
+        concat_basedir: '/dne',
       }
     end
 
@@ -13,7 +13,7 @@ describe 'dns::server::config', type: :class do
   end
 
   context 'on a Debian OS' do
-    let (:facts) do
+    let :facts do
       {
         osfamily: 'Debian',
         operatingsystemrelease: '6',
@@ -22,11 +22,11 @@ describe 'dns::server::config', type: :class do
     end
 
     it { is_expected.to contain_file('/etc/bind/').with_owner('bind') }
-    it { is_expected.to contain_file('/etc/bind/named.conf').with_content(/^include "\/etc\/bind\/named.conf.options";$/) }
+    it { is_expected.to contain_file('/etc/bind/named.conf').with_content(%r/^include "\/etc\/bind\/named.conf.options";$/) }
   end
 
   context 'on a RedHat OS' do
-    let (:facts) do
+    let :facts do
       {
         osfamily: 'RedHat',
         concat_basedir: '/dne',
@@ -34,6 +34,6 @@ describe 'dns::server::config', type: :class do
     end
 
     it { is_expected.to contain_file('/etc/named.conf').with_owner('named') }
-    it { is_expected.to contain_file('/etc/named.conf').with_content(/^include "\/etc\/named\/named.conf.options";$/) }
+    it { is_expected.to contain_file('/etc/named.conf').with_content(%r/^include "\/etc\/named\/named.conf.options";$/) }
   end
 end
